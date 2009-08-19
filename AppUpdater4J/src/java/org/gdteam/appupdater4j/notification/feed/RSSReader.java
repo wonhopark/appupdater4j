bbb12174720bc129158ee60e0a0fca768fe68e83
c.oukhemanou <c.oukhemanou@3cfa2d34-8c9b-11de-92d6-5bd20ad860ee>
2009-08-19 13:52:44 +0000

    Complete rss reader

--- AppUpdater4J/src/java/org/gdteam/appupdater4j/notification/feed/RSSReader.java
+++ AppUpdater4J/src/java/org/gdteam/appupdater4j/notification/feed/RSSReader.java
@@ -4,10 +4,13 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
+import java.util.Locale;
 
 import org.gdteam.appupdater4j.model.Application;
+import org.gdteam.appupdater4j.model.ApplicationVersion;
 import org.jdom.Element;
 
+import com.sun.syndication.feed.synd.SyndEntry;
 import com.sun.syndication.feed.synd.SyndFeed;
 import com.sun.syndication.io.FeedException;
 import com.sun.syndication.io.SyndFeedInput;
@@ -15,8 +18,15 @@ import com.sun.syndication.io.XmlReader;
 
 public class RSSReader {
     
-    public static final String CHANNEL_ID = "au:id";
-    public static final String CHANNEL_NAME = "au:name";
+    public static final String APP_ID = "au:id";
+    public static final String APP_NAME = "au:name";
+    
+    public static final String VERSION_MAJOR = "au:major";
+    public static final String VERSION_MINOR = "au:minor";
+    public static final String VERSION_BUILD = "au:build";
+    public static final String VERSION_REVISION = "au:revision";
+    public static final String VERSION_REBOOT = "au:reboot";
+    public static final String VERSION_DESCRIPTION = "au:description";
 
     /**
      * Get org.gdteam.appupdater4j.model.Application representation
@@ -36,12 +46,41 @@ public class RSSReader {
         
         List<Element> channelForeignMarkup = (List<Element>) feed.getForeignMarkup();
         for (Element element : channelForeignMarkup) {
-            if (element.getQualifiedName().equals(CHANNEL_ID)) {
+            if (element.getQualifiedName().equals(APP_ID)) {
                 ret.setId(element.getValue());
-            } else if (element.getQualifiedName().equals(CHANNEL_NAME)) {
+            } else if (element.getQualifiedName().equals(APP_NAME)) {
                 ret.setName(element.getValue());
             }
         }
+        
+        for (SyndEntry entry : ((List<SyndEntry>) feed.getEntries())) {
+            
+            ApplicationVersion version = new ApplicationVersion();
+            version.setName(entry.getTitle());
+            version.setPublicationDate(entry.getPublishedDate());
+            version.setUpdateURL(entry.getLink());
+            
+            List<Element> itemForeignMarkup = (List<Element>) entry.getForeignMarkup();
+            for (Element element : itemForeignMarkup) {
+                if (element.getQualifiedName().equals(VERSION_MAJOR)) {
+                    version.setMajor(element.getValue());
+                } else if (element.getQualifiedName().equals(VERSION_MINOR)) {
+                    version.setMinor(element.getValue());
+                } else if (element.getQualifiedName().equals(VERSION_BUILD)) {
+                    version.setBuild(element.getValue());
+                } else if (element.getQualifiedName().equals(VERSION_REVISION)) {
+                    version.setRevision(element.getValue());
+                } else if (element.getQualifiedName().equals(VERSION_REBOOT)) {
+                    version.setNeedReboot(Boolean.valueOf(element.getValue()));
+                } else if (element.getQualifiedName().equals(VERSION_DESCRIPTION)) {
+                    version.putDescription(new Locale(element.getAttributeValue("lang")), element.getValue());
+                }
+            }
+            
+            ret.addVersion(version);
+            
+        }
+        
         
         return ret;
     }
--- AppUpdater4J/test/java/org/gdteam/appupdater4j/notification/feed/RSSTest.java
+++ AppUpdater4J/test/java/org/gdteam/appupdater4j/notification/feed/RSSTest.java
@@ -1,10 +1,13 @@
 package org.gdteam.appupdater4j.notification.feed;
 
 import java.net.URL;
+import java.util.Date;
+import java.util.Locale;
 
 import junit.framework.Assert;
 
 import org.gdteam.appupdater4j.model.Application;
+import org.gdteam.appupdater4j.model.ApplicationVersion;
 import org.junit.Test;
 
 import com.sun.syndication.feed.synd.SyndEntry;
@@ -41,6 +44,57 @@ public class RSSTest {
             
             Assert.assertEquals("myappid", app.getId());
             Assert.assertEquals("myappname", app.getName());
+            
+            Locale fr = new Locale("fr");
+            Locale en = new Locale("en");
+            
+            //Firt
+            ApplicationVersion firstVersion = app.getVersions().get(0);
+            Assert.assertEquals("Version 0.0.4", firstVersion.getName());
+            Assert.assertEquals(new Date("19 Jul 2005 04:32:51 -0700").toString(), firstVersion.getPublicationDate().toString());
+            Assert.assertEquals("http://code.google.com/p/appupdater4j", firstVersion.getUpdateURL());
+            Assert.assertEquals("0", firstVersion.getMajor());
+            Assert.assertEquals("0", firstVersion.getMinor());
+            Assert.assertEquals("4", firstVersion.getBuild());
+            Assert.assertNull(firstVersion.getRevision());
+            Assert.assertEquals("Version initiale", firstVersion.getDescription(fr));
+            Assert.assertEquals("Initial version", firstVersion.getDescription(en));
+            
+            //Second
+            ApplicationVersion secondVersion = app.getVersions().get(1);
+            Assert.assertEquals("Version 0.0.5", secondVersion.getName());
+            Assert.assertEquals(new Date("19 Aug 2005 04:32:51 -0700").toString(), secondVersion.getPublicationDate().toString());
+            Assert.assertEquals("http://code.google.com/p/appupdater4j", secondVersion.getUpdateURL());
+            Assert.assertEquals("0", secondVersion.getMajor());
+            Assert.assertEquals("0", secondVersion.getMinor());
+            Assert.assertEquals("5", secondVersion.getBuild());
+            Assert.assertNull(secondVersion.getRevision());
+            Assert.assertEquals("Seconde version", secondVersion.getDescription(fr));
+            Assert.assertEquals("Second version", secondVersion.getDescription(en));
+            
+            //Third
+            ApplicationVersion thirdVersion = app.getVersions().get(2);
+            Assert.assertEquals("Version 0.0.5.1", thirdVersion.getName());
+            Assert.assertEquals(new Date("22 Aug 2005 04:32:51 -0700").toString(), thirdVersion.getPublicationDate().toString());
+            Assert.assertEquals("http://code.google.com/p/appupdater4j", thirdVersion.getUpdateURL());
+            Assert.assertEquals("0", thirdVersion.getMajor());
+            Assert.assertEquals("0", thirdVersion.getMinor());
+            Assert.assertEquals("5", thirdVersion.getBuild());
+            Assert.assertEquals("1", thirdVersion.getRevision());
+            Assert.assertEquals("Troisieme version", thirdVersion.getDescription(fr));
+            Assert.assertEquals("Third version", thirdVersion.getDescription(en));
+            
+            //Official
+            ApplicationVersion officialVersion = app.getVersions().get(3);
+            Assert.assertEquals("Version 1.0.0", officialVersion.getName());
+            Assert.assertEquals(new Date("14 Dec 2005 04:32:51 -0700").toString(), officialVersion.getPublicationDate().toString());
+            Assert.assertEquals("http://code.google.com/p/appupdater4j", officialVersion.getUpdateURL());
+            Assert.assertEquals("1", officialVersion.getMajor());
+            Assert.assertEquals("0", officialVersion.getMinor());
+            Assert.assertEquals("0", officialVersion.getBuild());
+            Assert.assertNull(officialVersion.getRevision());
+            Assert.assertEquals("Version officielle", officialVersion.getDescription(fr));
+            Assert.assertEquals("Official version", officialVersion.getDescription(en));
             
         } catch (Exception e) {
             e.printStackTrace();
